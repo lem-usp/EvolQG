15f9f307c66ff61ce25b7f72c4f86e4aac564200
diogro <diogro@gmail.com>
2013-09-18 19:07:12 -0300

    Removing call to rmvNorm2

--- R/MonteCarloR2.R
+++ R/MonteCarloR2.R
@@ -12,10 +12,12 @@ function (cov.matrix, sample.size, iterations = 1000)
   #   to the actual r2 value calculated from the original matrix. The
   #   remaining entries are values calculated from the resampling procedure.
 {
+  if(!require(mvtnorm))install.packages("mvtnorm")
+  library(mvtnorm)
   n.traits <- dim (cov.matrix) [1]
   populations <- list ()
   for (i in 1:iterations)
-    populations [[i]] <- rmvNorm2 (sample.size, sigma = cov.matrix, method = 'chol')
+    populations [[i]] <- rmvnorm (sample.size, sigma = cov.matrix, method = 'chol')
   it.matrices <- lapply (populations, cor)
   it.r2 <- sapply (it.matrices, CalcR2)
   r2 <- c (CalcR2 (cov2cor(cov.matrix)), it.r2)
