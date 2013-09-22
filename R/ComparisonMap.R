93c8d22b1c9c9dc534c0332838693c62e77c2ac6
diogro <diogro@gmail.com>
2013-09-22 17:14:46 -0300

    Polymorfic RandomSkewers

---
+++ R/ComparisonMap.R
@@ -0,0 +1,40 @@
+ComparisonMap <- function (matrix.list, MatrixCompFunc, repeat.vector = NULL, iterations = 10000)
+  # Performs multiple comparisons between a set of covariance or
+  # correlation matrices.
+  #
+  # Args:
+  #  matrix.list: a list of covariance or correlation matrices
+  #  MatrixCompFunc: function to use for comparison
+  #  repeat.vector: vector of matrix repeatabilities
+  #  iterations: number of RandomSkewers or matrix permutations passed to MatrixCompFunc
+  #
+  # Return:
+  #  a list with two matrices containing $\Gamma$-values or average random
+  #  skewers correlation and probabilities according to permutation test.
+  #  if repeat.vector was also passed, values below the diagonal on the correlation matrix
+  #  will contain corrected correlation values.
+{
+  n.matrix <- length (matrix.list)
+  matrix.names <- names (matrix.list)
+  probabilities <- array (0, c(n.matrix, n.matrix))
+  correlations <- probabilities
+  for (i in 1:(n.matrix - 1)) {
+    for (j in (i+1):n.matrix) {
+      comparing.now <- MatrixCompFunc (matrix.list [[i]],
+                                       matrix.list [[j]],
+                                       iterations)
+      correlations [i, j] <- comparing.now [1]
+      probabilities [i, j] <- comparing.now [2]
+      if (!is.null (repeat.vector))
+        correlations [j, i] <- correlations [i, j] / sqrt (repeat.vector [i] * repeat.vector [j])
+    }
+  }
+  if (!is.null (repeat.vector)) {
+    diag (correlations) <- repeat.vector
+  }
+  rownames (correlations) <- matrix.names
+  colnames (correlations) <- matrix.names
+  dimnames (probabilities) <- dimnames (correlations)
+  output <- list ('correlations' = correlations, 'probabilities' = probabilities)
+  return (output)
+}
--- R/RandomSkewers.R
+++ R/RandomSkewers.R
@@ -1,15 +1,16 @@
-RandomSkewers <-
-function (cov.matrix.1, cov.matrix.2, nsk = 10000)
+RandomSkewers <- function(x, ...) UseMethod("RandomSkewers")
+
+RandomSkewers.default <- function (cov.matrix.1, cov.matrix.2, iterations = 10000)
   # Calculates covariance matrix correlation via random skewers
   # Args:
   #     cov.matrix.(1,2): Two covariance matrices to be compared
-  #     nsk: Number of generated random skewers
+  #     iterations: Number of generated random skewers
   # Return:
   #     List with mean value of correlation, p value and standard deviation
 {
   traits <- dim (cov.matrix.1) [1]
   base.vector <- Normalize(rnorm(traits))
-  random.vectors <- array (rnorm (nsk * traits, mean = 0, sd = 1), c(traits, nsk))
+  random.vectors <- array (rnorm (iterations * traits, mean = 0, sd = 1), c(traits, iterations))
   random.vectors <- apply (random.vectors, 2, Normalize)
   dist <- base.vector %*% random.vectors
   dz1 <- apply (cov.matrix.1 %*% random.vectors, 2, Normalize)
@@ -17,8 +18,15 @@ function (cov.matrix.1, cov.matrix.2, nsk = 10000)
   real <- apply (dz1 * dz2, 2, sum)
   ac <- mean (real)
   stdev <- sd (real)
-  prob <- sum (ac < dist) / nsk
+  prob <- sum (ac < dist) / iterations
   output <- c(ac, prob, stdev)
   names(output) <- c("AC","P","SD")
   return(output)
 }
+
+RandomSkewers.list <- function (matrix.list, repeat.vector = NULL, iterations)
+{
+    out <- ComparisonMap(matrix.list, RandomSkewers.default, repeat.vector, iterations)
+    return(out)
+}
+
