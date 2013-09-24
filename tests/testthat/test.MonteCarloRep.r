07746967d757c5319e11cd3c971a9f509cb9e9b8
diogro <diogro@gmail.com>
2013-09-24 15:20:49 -0300

    MonteCarloRep tests

--- tests/testthat/test.MonteCarloRep.r
+++ tests/testthat/test.MonteCarloRep.r
@@ -1,5 +1,9 @@
 test_that("MonteCarloRep returns sensible results",
           {
-            expect_that(MonteCarloRep(diag(rep(2, 10)), 10) <= 1, is_true())
+            cov.matrix <- RandomMatrix(10, 1, 1, 10)
+            expect_that(MonteCarloRepRandomSkewers(cov.matrix, 30) <= 1, is_true())
+            expect_that(MonteCarloRepMantelCor(cov.matrix, 30) <= 1, is_true())
+            expect_that(MonteCarloRepKrzCor(cov.matrix, 30) <= 1, is_true())
+            expect_that(MonteCarloRepKrzCor(cov.matrix, 30, T) <= 1, is_true())
           }
 )
