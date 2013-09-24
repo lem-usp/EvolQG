27feeb4c831d063d79f3f15249a2a143036691ab
diogro <diogro@gmail.com>
2013-09-23 21:11:35 -0300

    KzrCor rarefaction cor tests

--- tests/testthat/test.Rarefaction.r
+++ tests/testthat/test.Rarefaction.r
@@ -25,3 +25,12 @@ test_that("RarefactionKrzCor returns sensible results",
             expect_that(length(results[[1]]), equals(5))
           }
 )
+test_that("RarefactionKrzCor returns sensible results",
+          {
+            ind.data <- matrix(rnorm(30*10), 30, 10)
+            results <- RarefactionKrzCor(ind.data, T, num.reps = 5)
+            expect_that(results, is_a("list"))
+            expect_that(names(results), equals(as.character(1:29)))
+            expect_that(length(results[[1]]), equals(5))
+          }
+)
