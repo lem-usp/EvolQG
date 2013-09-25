3fcd75f71b75c9d9c0bd9507f0db329ffaeaf94c
diogro <diogro@gmail.com>
2013-09-24 22:42:05 -0300

    Usable MeanMatrixStatistics

--- R/MatrixStatistics.R
+++ R/MatrixStatistics.R
@@ -1,12 +1,23 @@
-Autonomy <- function (beta, cov.matrix) return (((t (beta) %*% solve (cov.matrix, beta))^(-1)) / (t (beta) %*% cov.matrix %*% beta))
-ConditionalEvolvability <- function (beta, cov.matrix) return ((t (beta) %*% solve (cov.matrix) %*% beta)^(-1))
+Autonomy <- function (beta, cov.matrix) return ((1/(t (beta) %*% solve (cov.matrix, beta))) / (t (beta) %*% cov.matrix %*% beta))
+ConditionalEvolvability <- function (beta, cov.matrix) return (1/(t (beta) %*% solve (cov.matrix, beta)))
 Constraints <- function (beta, cov.matrix) return (abs (t (Normalize (eigen (cov.matrix)$vectors[,1])) %*% Normalize (cov.matrix %*% beta)))
 Evolvability <- function (beta, cov.matrix) return (t (beta) %*% cov.matrix %*% beta)
 Flexibility <- function (beta, cov.matrix) return (t (beta) %*% cov.matrix %*% beta / Norm (cov.matrix %*% beta))
 Pc1Percent <- function (cov.matrix) return (eigen (cov.matrix)$values [1] / sum (eigen (cov.matrix)$values))
 Respondability <- function (beta, cov.matrix) return (Norm (cov.matrix %*% beta))
 
-MeanMatrixStatistics <- function (cov.matrix, iterations = 10000) {
+MeanMatrixStatistics <- function (cov.matrix, iterations = 10000, full.results = F, num.cores = 1) {
+  library(plyr)
+  if (num.cores > 1) {
+    library(doMC)
+    library(foreach)
+    registerDoMC(num.cores)
+    parallel = TRUE
+  }
+  else{
+    parallel = FALSE
+  }
+
   matrix.stat.functions = list ('respondability' = Respondability,
                                 'evolvability' = Evolvability,
                                 'conditional.evolvability' = ConditionalEvolvability,
@@ -16,16 +27,14 @@ MeanMatrixStatistics <- function (cov.matrix, iterations = 10000) {
   num.traits <- dim (cov.matrix) [1]
   beta.mat <- array (rnorm (num.traits * iterations), c(num.traits, iterations))
   beta.mat <- apply (beta.mat, 2, Normalize)
-  iso.vec <- Normalize (rep(1, times = num.traits))
+  iso.vec <- Normalize (rep(1, num.traits))
   null.dist <- abs (t (iso.vec) %*% beta.mat)
   null.dist <- sort (null.dist)
   crit.value <- null.dist [round (0.95 * iterations)]
   cat ('critical value: ', crit.value, '\n')
-  stat.dist <- array (0, c(iterations, 8))
-  MatrixStatisticsMap <- function (CurrentFunc) return (apply (beta.mat, 2, CurrentFunc, cov.matrix = cov.matrix))
-  stat.dist [,1:6] <- sapply (matrix.stat.functions, MatrixStatisticsMap)
-  stat.dist[,7] <- as.numeric (stat.dist[,5] > crit.value)
-  stat.dist[,8] <- as.numeric (stat.dist[,6] > crit.value)
+  stat.dist <- array (0, c(iterations, 6))
+  MatrixStatisticsMap <- function (CurrentFunc) return (aaply (beta.mat, 2, CurrentFunc, cov.matrix = cov.matrix))
+  stat.dist [,1:6] <- laply (matrix.stat.functions, MatrixStatisticsMap, .parallel = parallel)
   stat.dist <- cbind (stat.dist, null.dist)
   colnames (stat.dist) <- c('respondability',
                             'evolvability',
@@ -33,16 +42,13 @@ MeanMatrixStatistics <- function (cov.matrix, iterations = 10000) {
                             'autonomy',
                             'flexibility',
                             'constraints',
-                            'flex.n',
-                            'const.n',
                             'null.dist')
-  stat.mean <- colMeans (stat.dist)
-  stat.mean[7:8] <- stat.mean[7:8] * iterations
-  pc1 <- eigen (cov.matrix)$vectors[,1]
-  MatrixStatisticsMapPc1 <- function (CurrentFunc) return (CurrentFunc (beta = pc1, cov.matrix = cov.matrix))
-  maximum <- sapply (matrix.stat.functions, MatrixStatisticsMapPc1)
+  stat.mean <- colMeans (stat.dist[,-7])
   integration <- c (CalcR2 (cov.matrix), Pc1Percent (cov.matrix))
   names (integration) <- c ('MeanSquaredCorrelation', 'pc1%')
   stat.mean <- c (integration, stat.mean)
-  return (list ('dist' = stat.dist, 'mean' = stat.mean, 'max.val' = maximum))
+  if(full.results)
+    return (list ('dist' = stat.dist, 'mean' = stat.mean))
+  else
+    return (stat.mean)
 }
