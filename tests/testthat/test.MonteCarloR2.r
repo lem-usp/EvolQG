5aa66c0c126aba0da6061a995115cd5406cd1bee
Diogo Melo <diogro@gmail.com>
2013-09-27 18:33:48 -0300

    Merged montecarlo tests

--- tests/testthat/test.MonteCarloR2.r
+++
@@ -1,9 +0,0 @@
-test_that("MonteCarloR2 returns sensible results",
-          {
-            expect_that(length(MonteCarloR2(diag(rep(1, 10)), 10)), equals(1000))
-            expect_that(length(MonteCarloR2(diag(rep(1, 10)), 10, 10)), equals(10))
-            results <- MonteCarloR2(diag(rep(1, 10)), 10)
-            corrs = sapply(results, function(x) isTRUE(x < 1 & x > 0))
-            expect_that(sum(corrs), equals(1000))
-          }
-)
--- tests/testthat/test.MonteCarloStat.r
+++ tests/testthat/test.MonteCarloStat.r
@@ -1,3 +1,13 @@
+test_that("MonteCarloR2 returns sensible results",
+          {
+            expect_that(length(MonteCarloR2(diag(rep(2, 10)), 10)), equals(1000))
+            expect_that(length(MonteCarloR2(diag(rep(2, 10)), 10, 10)), equals(10))
+            results <- MonteCarloR2(diag(rep(2, 10)), 10)
+            corrs = sapply(results, function(x) isTRUE(x < 1 & x > 0))
+            expect_that(sum(corrs), equals(1000))
+            expect_that(RandomMatrix(10), gives_warning("Matrix appears to be a correlation matrix! Only covariance matrices should be used in parametric resampling."))
+          }
+)
 test_that("MonteCarloRep returns sensible results",
           {
             cov.matrix <- RandomMatrix(10, 1, 1, 10)
