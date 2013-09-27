fbc2c40e85dad85c7ea2bc5ced356617d2b92039
Diogo Melo <diogro@gmail.com>
2013-09-27 17:54:24 -0300

    test for passing correlation matrix to parametric montecarlo function

--- tests/testthat/test.MonteCarloRep.r
+++ tests/testthat/test.MonteCarloRep.r
@@ -1,6 +1,7 @@
 test_that("MonteCarloRep returns sensible results",
           {
             cov.matrix <- RandomMatrix(10, 1, 1, 10)
+            expect_that(MonteCarloRepRandomSkewers(RandomMatrix(10), 30), gives_warning("Matrix appears to be a correlation matrix! Only covariance matrices should be used in parametric resampling."))
             expect_that(MonteCarloRepRandomSkewers(cov.matrix, 30) <= 1, is_true())
             expect_that(MonteCarloRepMantelCor(cov.matrix, 30) <= 1, is_true())
             expect_that(MonteCarloRepKrzCor(cov.matrix, 30) <= 1, is_true())
