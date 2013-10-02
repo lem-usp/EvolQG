cabfbca8758738366d1f359e3d0e13db4235f319
diogro <diogro@gmail.com>
2013-10-01 22:46:41 -0300

    AlphaRep clean up and docs

--- R/AlphaRep.R
+++ R/AlphaRep.R
@@ -1,16 +1,6 @@
-AlphaRep <- function (cor.matrix, tam)
-  # Calculates the matrix repeatability using the equation in Cheverud 1996
-  # Quantitative genetic analysis of cranial morphology in the cotton-top
-  # (Saguinus oedipus) and saddle-back (S. fuscicollis) tamarins. Journal of Evolutionary Biology 9, 5-42.
-  #
-  # Args:
-  #     cor.matrix: correlation matrix
-  #     tam: sample size
-  # Return:
-  #     matrix repeatability
-{
+AlphaRep <- function (cor.matrix, sample.size){
   vec <- cor.matrix[lower.tri(cor.matrix)]
-  var.erro <- (1 - mean(vec)^2)/(tam-2)
+  var.erro <- (1 - mean(vec)^2)/(sample.size-2)
   var.vec <- var(vec)
   return((var.vec - var.erro)/var.vec)
 }
