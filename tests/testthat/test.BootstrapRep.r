00f02d8e91d97b53f37c6b35b13751b638e61302
Diogo Melo <diogro@gmail.com>
2014-04-14 11:07:02 +0100

    Generic BootstrapRep using match.arg and swich. Specific BootSrap functions no longer exported.

--- tests/testthat/test.BootstrapRep.r
+++ tests/testthat/test.BootstrapRep.r
@@ -1,16 +1,16 @@
 test_that("BootstrapRep returns sensible results",
           {
               ind.data <- iris[,1:4]
-              expect_that(BootstrapRepRandomSkewers(ind.data, 50) <= 1 , is_true())
-              expect_that(BootstrapRepMantelCor(ind.data, 50) <= 1 , is_true())
-              expect_that(BootstrapRepKrzCor(ind.data, 50) <= 1 , is_true())
-              expect_that(BootstrapRepKrzCor(ind.data, 50, T) <= 1 , is_true())
-              expect_that(BootstrapRepRandomSkewers(ind.data, 50) >= 0.98 , is_true())
-              expect_that(BootstrapRepRandomSkewers(ind.data, 50, T) >= 0.98 , is_true())
-              expect_that(BootstrapRepMantelCor(ind.data, 50) >= 0.98 , is_true())
-              expect_that(BootstrapRepKrzCor(ind.data, 50) >= 0.98 , is_true())
-              expect_that(BootstrapRepKrzCor(ind.data, 50, T) >= 0.98 , is_true())
-              expect_that(BootstrapRepRandomSkewers(cov(ind.data), 50), throws_error("input appears to be a matrix, use residuals."))
-              expect_that(BootstrapRepRandomSkewers(cor(ind.data), 50), throws_error("input appears to be a matrix, use residuals."))
+              expect_that(BootstrapRep(ind.data, "randomskewers", 50) <= 1 , is_true())
+              expect_that(BootstrapRep(ind.data, "mantel", 50) <= 1 , is_true())
+              expect_that(BootstrapRep(ind.data, "krz", 50) <= 1 , is_true())
+              expect_that(BootstrapRep(ind.data, "krz", 50, T) <= 1 , is_true())
+              expect_that(BootstrapRep(ind.data, "randomskewers", 50) >= 0.98 , is_true())
+              expect_that(BootstrapRep(ind.data, "randomskewers", 50, T) >= 0.98 , is_true())
+              expect_that(BootstrapRep(ind.data, "mantel", 50) >= 0.98 , is_true())
+              expect_that(BootstrapRep(ind.data, "krz", 50) >= 0.98 , is_true())
+              expect_that(BootstrapRep(ind.data, "krz", 50, T) >= 0.98 , is_true())
+              expect_that(BootstrapRep(cov(ind.data), "randomskewers", 50), throws_error("input appears to be a matrix, use residuals."))
+              expect_that(BootstrapRep(cor(ind.data), "randomskewers", 50), throws_error("input appears to be a matrix, use residuals."))
           }
 )
