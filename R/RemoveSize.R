0a529d67c90876f5ce81f2ff8c9ccf38a041e2db
aivuk <edgar@ymonad.com>
2015-01-20 16:09:19 -0200

    Minor typo correction

--- R/RemoveSize.R
+++ R/RemoveSize.R
@@ -4,7 +4,7 @@
 #'
 #' @param cov.matrix Covariance matrix
 #' @details Function sets the first eigen value to zero.
-#' @return Altered covariance matriz, with no variation on former first principal component
+#' @return Altered covariance matrix with no variation on former first principal component
 #' @author Diogo Melo, Guilherme Garcia
 #' @export
 #' @examples
