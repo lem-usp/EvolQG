8af7a31a66044686162c161ce719468c5dd913fa
diogro <diogro@gmail.com>
2015-02-10 16:39:40 -0200

    Added CalcICV

--- R/CalcR2.R
+++ R/CalcR2.R
@@ -7,6 +7,8 @@
 #' @export
 #' @author Diogo Melo, Guilherme Garcia
 #' @seealso \code{\link{Flexibility}}
+#' @references Porto, Arthur, Felipe B. de Oliveira, Leila T. Shirai, Valderes de Conto, and Gabriel Marroig. 2009. "The Evolution of Modularity in the Mammalian Skull I: Morphological Integration Patterns and Magnitudes." Evolutionary Biology 36 (1): 118-35. doi:10.1007/s11692-008-9038-3.
+#' @references Porto, Arthur, Leila Teruko Shirai, Felipe Bandoni de Oliveira, and Gabriel Marroig. 2013. "Size Variation, Growth Strategies, and the Evolution of Modularity in the Mammalian Skull." Evolution 67 (July): 3305-22. doi:10.1111/evo.12177.
 #' @examples
 #' cov.matrix <- RandomMatrix(10, 1, 1, 10)
 #' # both of the following calls are equivalent, 
