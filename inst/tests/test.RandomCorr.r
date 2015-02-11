52b5580ef0cd352da31d4e44ce932c207bea9fbd
diogro <diogro@gmail.com>
2015-02-11 13:01:42 -0200

    RandomMatrix tests

--- inst/tests/test.RandomCorr.r
+++ inst/tests/test.RandomCorr.r
@@ -1,10 +1,10 @@
 test_that("RandomCorr returns sensible results",
           {
-            mat.list <- lapply(as.list(1:10), function(x) RandomMatrix(10))
-            mean.corr <- mean(MantelCor(mat.list, iterations=1)[[1]][upper.tri(array(0, c(100, 100)))])
+            mat.list <- lapply(as.list(1:100), function(x) RandomMatrix(10))
+            mean.corr <- mean(MatrixCor(mat.list)[upper.tri(array(0, c(100, 100)))])
             expect_that(dim(mat.list[[1]]), equals(c(10, 10)))
             expect_that(sum(diag(mat.list[[1]])), equals(10))
-            #expect_that(abs(mean.corr) < 0.01, is_true())
+            expect_that(abs(mean.corr) < 0.01, is_true())
           }
 )
 
