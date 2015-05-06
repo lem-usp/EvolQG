80080761bc618241fa8e8816aafd8c84ed04ca9d
diogro <diogro@gmail.com>
2015-05-06 18:25:19 -0300

    passes check

--- R/SRD.R
+++ R/SRD.R
@@ -9,7 +9,7 @@
 #' @param num.cores If list is passed, number of threads to use in computation.
 #' The doMC library must be loaded.
 #' @param ... aditional parameters passed to other methods
-#' @param output Output from SRD function, used in ploting
+#' @param x Output from SRD function, used in ploting
 #' @param matrix.label Plot label
 #' @details Output can be ploted using PlotSRD function
 #' @return List of SRD scores means, confidence intervals, standard
@@ -138,7 +138,8 @@ SRD.list <- function (cov.x, cov.y = NULL, iterations = 1000, num.cores = 1, ...
 
 #' @rdname SRD
 #' @export
-plot.SRD <- function (output, matrix.label = "") {
+plot.SRD <- function (x, matrix.label = "", ...) {
+  output <- x
   layout (array (c(1,1,2,2),c(2,2)))
   par (mar = c(4.0, 4.0, 8, 0.4))
   mean.r2 <- output$output[,1]
@@ -154,7 +155,7 @@ plot.SRD <- function (output, matrix.label = "") {
   ### b l t r
   pc.pch <- output$model$code + 20
   plot (mean.r2, type = "p", lty = 2, pch = pc.pch,
-        ylab = "", xlab = "", xaxt = "n", ylim = c(-1,1))
+        ylab = "", xlab = "", xaxt = "n", ylim = c(-1,1), ...)
   for (i in 1:length (mean.r2))
   {
     abline (v = i, lty = 1, col = rgb (0.8,0.8,0.8))
@@ -173,7 +174,7 @@ plot.SRD <- function (output, matrix.label = "") {
   output$model$code[output$model$code == 0] <- 1
   pc.cex <- output$model$code
   plot (c.sd.r2 ~ c.mean.r2, pch = pc.pch, xlab = "", ylab = "",
-        yaxt = "n", main = matrix.label, cex.main= 3, cex.axis=1.3)
+        yaxt = "n", main = matrix.label, cex.main= 3, cex.axis=1.3, ...)
   abline (v = 0, lty = 2)
   abline (h = 0, lty = 2)
   text (c.mean.r2, c.sd.r2, labels = dists, pos = 4, cex = pc.cex)
