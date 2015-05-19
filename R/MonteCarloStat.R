059f03154c89b21d55c530ed0022174da5f5b6ac
diogro <diogro@gmail.com>
2015-05-19 15:37:28 -0300

    removing doMC completely

--- R/MonteCarloStat.R
+++ R/MonteCarloStat.R
@@ -30,12 +30,17 @@
 #' r2.dist <- MonteCarloR2(RandomMatrix(10, 1, 1, 10), 30)
 #' quantile(r2.dist)
 #' 
-#' #Multiple threads can be used with doMC library
-#' library(doMC)
-#' MonteCarloStat(cov.matrix, sample.size = 30, iterations = 100,
-#'                ComparisonFunc = function(x, y) RandomSkewers(x, y)[1],
-#'                StatFunc = cov,
-#'                parallel = TRUE)
+#' #Multiple threads can be used with some foreach backend library, like doMC or doParallel
+#' #library(doParallel)
+#' ##Windows:
+#' #cl <- makeCluster(2)
+#' #registerDoParallel(cl)
+#' ##Mac and Linux:
+#' #registerDoParallel(cores = 2)
+#' #MonteCarloStat(cov.matrix, sample.size = 30, iterations = 100,
+#' #               ComparisonFunc = function(x, y) RandomSkewers(x, y)[1],
+#' #               StatFunc = cov,
+#' #               parallel = TRUE)
 #' @keywords parametricsampling
 #' @keywords montecarlo
 #' @keywords repeatability
