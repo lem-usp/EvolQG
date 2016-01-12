19c230e79ddcce2e33b1e006eae7b401ca26d6c8
Diogo Melo <diogro@gmail.com>
2016-01-12 19:33:05 -0200

    initial tests for baysean matrix estimation out of the box.

--- R/CalculateMatrix.R
+++ R/CalculateMatrix.R
@@ -20,3 +20,24 @@ CalculateMatrix <- function(linear.m){
   cov.matrix = var(linear.m$residuals)*((dim(linear.m$residuals)[1]-1)/linear.m$df.residual)
   return (cov.matrix)
 }
+
+#' @importFrom MCMCpack riwish
+CalculateMatrix_Baysean <- function(linear.m, samples = NULL, ..., nu = NULL, S_0 = NULL){
+  x <- linear.m$residuals
+  S_x = t(x) %*% x
+  N = dim(x)[1]
+  p = dim(x)[2]
+  if(is.null(nu)) nu = dim(x)[2] + 1
+  if(is.null(S_0)){
+    S_0 = diag(0, p)
+    diag(S_0) = diag(S_x/N) * nu
+  }
+  S_N <- S_0 + S_x
+  nu_N <- nu + N
+  MAP <- (S_0 + S_x) / (nu + N)
+  if(!is.null(samples)){
+   S_sample <- rlply(samples, riwish(nu_N, S_N))
+   return(list(MAP = MAP, posterior_samples = S_sample))
+  }
+  else return(MAP)
+}
