d3be1239c82110442f147f44fd1d43c50f9c9549
diogro <diogro@gmail.com>
2016-01-15 16:20:52 -0200

    me spel gud

--- R/CalculateMatrix.R
+++ R/CalculateMatrix.R
@@ -21,8 +21,30 @@ CalculateMatrix <- function(linear.m){
   return (cov.matrix)
 }
 
+#' Calculate Covariance Matrix from a linear model fitted with lm() using different estimators
+#'
+#' Calculates covariance matrix using the maximum likelihood estimator, the maximum a posteriori (MAP)
+#' estimator under a regularized Wishart prior, and if the sample is large enough can give samples from the 
+#' posterior and the median posterior estimator.
+#' @param linear.m Linear model adjusted for original data
+#' @param samples number os samples to be generated from the posterior. Requires sample size to be at least as large as the number of dimensions
+#' @param ... aditional arguments, currently ignored
+#' @param nu degrees of freedom in prior distribution, defaults to the number of traits (this can be a too strong prior)
+#' @param S_0 cross product matrix of the prior. Default is to use the observed voriances and zero covariances
+#'
+#' @return Estimated covariance matrices and posterior samples
+#' @author Diogo Melo, Fabio Machado
+#' @references Murphy, K. P. (2012). Machine learning: a probabilistic perspective. MIT press.
+#' @references Schafer, J., e Strimmer, K. (2005). A shrinkage approach to large-scale covariance matrix estimation and implications for functional genomics. Statistical applications in genetics and molecular biology, 4(1).
+#' @export
+#' @examples
+#' data(iris)
+#' iris.lm = lm(as.matrix(iris[,1:4])~iris[,5])
+#' matrices <- BayesianCalculateMatrix(iris.lm, nu = 0.1, samples = 100)
+#'
+#' @keywords covariancematrix
 #' @importFrom MCMCpack riwish
-CalculateMatrix_Baysean <- function(linear.m, samples = NULL, ..., nu = NULL, S_0 = NULL){
+BayesianCalculateMatrix <- function(linear.m, samples = NULL, ..., nu = NULL, S_0 = NULL){
   x <- linear.m$residuals
   S_x = t(x) %*% x
   N = linear.m$df.residual
