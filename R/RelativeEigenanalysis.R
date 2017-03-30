a527762dfc641831326ecf6231473ffd98746ca4
Guilherme Garcia <wgar@usp.br>
2017-03-30 16:45:08 -0300

    started implementing Relative Eigenanalysis

---
+++ R/RelativeEigenanalysis.R
@@ -0,0 +1,38 @@
+#' Relative Eigenanalysis
+#'
+#' Computes relative eigenvalues and eigenvectors between a pair of covariance matrices.
+#'
+#' @param cov.x covariance matrix
+#' @param cov.y covariance matrix
+#' @param symmetric compute symmetric eigenanalysis?
+#' @return list with two objects: eigenvalues and eigenvectors
+#'
+#' @importFrom expm sqrtm
+#'
+#' 
+#' @rdname RelativeEigenanalysis
+#'
+#' @references Bookstein, Fred L., and Philipp Mitteroecker. "Comparing Covariance Matrices by
+#' Relative Eigenanalysis, with Applications to Organismal Biology." Evolutionary Biology 41, no. 2
+#' (June 1, 2014): 336–50. doi:10.1007/s11692-013-9260-5.
+#'
+#' @author Guilherme Garcia, Diogo Melo
+#'
+#' @examples
+#' data(dentus)
+#' dentus.vcv <- dlply(dentus, .(species), function(df) var(df[, -5]))
+#'
+#' dentus.eigrel <- RelativeEigenanalysis(dentus.vcv [[1]], denntus.vcv[[5]])
+#' 
+RelativeEigenanalysis <- function(cov.x, cov.y, symmetric = FALSE)
+{
+    if(symmetric)
+    {
+        out.elem <- solve(sqrtm(cov.y))
+        matrix.prod <- out.elem %*% cov.x %*% out.elem
+    }
+    else
+        matrix.prod <- cov.x %*% solve(cov.y)
+
+    eigen(matrix.prod)
+}   
